6a6695eab523448ad6be9761f7bd067f3e6fe0af
Boris Charney <boris.charney@gmail.com>
2022-08-16 19:25:02 -0700

    commit tree 8714194c9f31ce780ee50b81642ff23acc8cb8ac

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(){
+   return appendCat = [...cats, "Broom"];
+}
+
+function prependCat(){
+   return prependCat = ["Arnold", ...cats];
+}
+
+function removeLastCat(){
+   return cats.slice(0, -1);
+}
+
+function removeFirstCat(){
+    return cats.slice(1);
+ }
